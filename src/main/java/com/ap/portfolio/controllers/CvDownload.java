709d2505ee21841931ac8beae2af6af8faefce21
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2023-05-15 21:05:15 -0300

    fixed cv download

--- src/main/java/com/ap/portfolio/controllers/CvDownload.java
+++ src/main/java/com/ap/portfolio/controllers/CvDownload.java
@@ -13,29 +13,26 @@ import java.io.*;
 public class CvDownload {
     @GetMapping("/download")
     public void downloadCv(HttpServletResponse response) throws IOException {
-            File cv = new File("src/main/java/com/ap/portfolio/utilities/cv/SantillanLautaroNahuelJrJavaDev.pdf");
+        File cv = new File("src/main/java/com/ap/portfolio/utilities/cv/SantillanLautaroNahuelJrJavaDev.pdf");
 
-        response.setContentType("application/octect-stream");
+        response.setContentType("application/octet-stream");
         String headerKey = "Content-Disposition";
-        String headerValue= "attachment; filename=" + cv.getName();
-
+        String headerValue = "attachment; filename=" + cv.getName();
         response.setHeader(headerKey, headerValue);
 
         ServletOutputStream outputStream = response.getOutputStream();
-
         BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(cv));
 
         byte[] buffer = new byte[8192];
-        int bytesRead =-1;
+        int bytesRead;
 
-        while ((bytesRead = inputStream.read(buffer)) != -1){
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
             outputStream.write(buffer, 0, bytesRead);
-
-            inputStream.close();
-            outputStream.close();
         }
+
+        inputStream.close();
+        outputStream.close();
     }
-
-
 }
+
 
